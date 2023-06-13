b8f333663bbee6b10317796b7d67971fc1180c91
Shahadat Jaman <devloperabuhuraira@gmail.com>
2023-06-13 16:18:59 +0600

    dummy data of battle page

---
+++ src/pages/battle__chart/right___content/data.js
@@ -0,0 +1,26 @@
+export const user1 = {
+  avatar:
+    "https://res.cloudinary.com/dza2t1htw/image/upload/v1686478582/40423_n8ceeq.jpg",
+  name: "Shahadat Jaman",
+};
+
+export const user2 = {
+  avatar:
+    "https://res.cloudinary.com/dza2t1htw/image/upload/v1686384127/20842_th1j1e.jpg",
+  name: "Amalia",
+};
+
+export const categories = [
+  {
+    name: "all",
+    value: 1,
+  },
+  {
+    name: "open",
+    value: 7,
+  },
+  {
+    name: "close",
+    value: 1,
+  },
+];
